e9ff9317b54091ee1d00d3559a447d1b72b80f93
Lanz <hanaarena@gmail.com>
2016-02-27 18:46:58 +0800

    init repo

--- build/com.cmd
+++
@@ -1,4 +0,0 @@
-java -jar compiler.jar ..\combined\alloyimage.js --js_output_file=..\combined\temp.js
-del ..\combined\alloyimage.js
-rename ..\combined\temp.js alloyimage.js
-del ..\combined\temp.js
